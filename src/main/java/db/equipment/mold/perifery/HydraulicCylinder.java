e3b5fd4aa0b9662686ba5138e07f7f81c104fbc0
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-04-15 20:03:51 +0300

    added fields mounted & mountedTo

--- src/main/java/db/equipment/mold/perifery/HydraulicCylinder.java
+++ src/main/java/db/equipment/mold/perifery/HydraulicCylinder.java
@@ -1,6 +1,9 @@
 package db.equipment.mold.perifery;
 
 import db.equipment.Equipment;
+import db.equipment.injectionmoldingmachine.InjectionMoldingMachine;
+import db.equipment.mold.MoldInsert;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 
@@ -13,6 +16,28 @@ public class HydraulicCylinder extends Equipment {
     private int cylinderLengthMm;
     @Column(name = "cylinder_width_mm")
     private int cylinderWidthMm;
+    @Column(name = "cylinder_mounted")
+    @ColumnDefault("true")
+    private boolean cylinderMounted;
+    @ManyToOne(targetEntity = MoldInsert.class)
+    @JoinColumn(name = "cylinder_mounted_to", referencedColumnName = "equipment_id")
+    private MoldInsert cylinderMountedTo;
+
+    public boolean isCylinderMounted() {
+        return cylinderMounted;
+    }
+
+    public void setCylinderMounted(boolean cylinderMounted) {
+        this.cylinderMounted = cylinderMounted;
+    }
+
+    public MoldInsert getCylinderMountedTo() {
+        return cylinderMountedTo;
+    }
+
+    public void setCylinderMountedTo(MoldInsert cylinderMountedTo) {
+        this.cylinderMountedTo = cylinderMountedTo;
+    }
 
     public int getStrokeLengthMm() {
         return strokeLengthMm;
